abb30cc81e6502c58525f0960fba0919cbe63bbd
Ilkin Alakbarli <78841212+thealekberli@users.noreply.github.com>
2023-06-29 02:50:56 +0400

    Create problem8570.java

---
+++ problem8570.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Main
+{
+    public static void main(String args[])
+    {
+
+        Scanner sc = new Scanner(System.in);
+        String s;
+        while(sc.hasNext())
+        {
+            s = sc.next();
+            System.out.print(s.length()+" ");
+        }
+    }
+}
+
